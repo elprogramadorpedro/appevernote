ebcdbac64c4f5bab0341b19b65873f0922dac822
Pedro <elprogramadorpedro@gmail.com>
2021-10-27 13:41:28 -0300

    Adding the header. For the application.

---
+++ javascript_note/src/App.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from "react";
+import Header from "./components/header";
+import "./App.scss";
+
+const App = () => {
+  return (
+    <Fragment>
+      <Header />
+    </Fragment>
+  );
+};
+
+export default App;
