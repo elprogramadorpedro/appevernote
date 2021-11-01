d04db476f0740abbe6cf6ba8dea15f5995cd2eeb
Pedro <elprogramadorpedro@gmail.com>
2021-11-01 18:20:04 -0300

    component header logout

--- javascript_note/src/screens/notes/index/index.js
+++ javascript_note/src/screens/notes/index/index.js
@@ -1,9 +1,12 @@
 import React, { Fragment } from 'react';
+import HeaderLogged from '../../../components/header_logged';
 
 const NoteScreen = () => (
   <Fragment>
+    <HeaderLogged/>
     Hello World
     </Fragment>
 );
 
 export default NoteScreen;
+
